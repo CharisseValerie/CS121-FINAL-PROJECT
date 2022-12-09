4c0261359e8c04b4480da3fb0feffc918b3da9f5
Patrick Jerome R. Rosales <112956946+PJRosales@users.noreply.github.com>
2022-12-10 05:33:36 +0800

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,34 @@
+class MenuItem:
+    """Models each Menu Item."""
+    def __init__(self, name, water, milk, coffee, cost):
+        self.name = name
+        self.cost = cost
+        self.ingredients = {
+            "water": water,
+            "milk": milk,
+            "coffee": coffee
+        }
+
+
+class Menu:
+    """Models the Menu with drinks."""
+    def __init__(self):
+        self.menu = [
+            MenuItem(name="choco", water=100, milk=150, coffee=30, cost=10.50),
+            MenuItem(name="mocha", water=100, milk=0, coffee=30, cost=12.50),
+            MenuItem(name="cappuccino", water=100, milk=50, coffee=40, cost=15.00),
+        ]
+
+    def get_items(self):
+        """Returns all the names of the available menu items"""
+        options = ""
+        for item in self.menu:
+            options += f"{item.name}/"
+        return options
+
+    def find_drink(self, order_name):
+        """Searches the menu for a particular drink by name. Returns that item if it exists, otherwise returns None"""
+        for item in self.menu:
+            if item.name == order_name:
+                return item
+        print("Sorry that item is not available.")
